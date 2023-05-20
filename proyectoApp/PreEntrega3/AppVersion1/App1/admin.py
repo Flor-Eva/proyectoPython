b0568fd281bb60519af0a96618b6a84d7e7f6660
Flor Espe <espejo.florencia@gmail.com>
2023-05-20 20:20:40 -0300

    proyecto final V1

--- proyectoApp/PreEntrega3/AppVersion1/App1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-admin.site.register(Acceso)
-admin.site.register(Freelance)
-admin.site.register(Contratador)
-admin.site.register(Servicios)
